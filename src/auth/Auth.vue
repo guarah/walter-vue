414c6ccff4ad4ae6c387e54c19c8ab3d5434f902
Guaratã Alencar <guara_st@hotmail.com>
2017-11-09 21:22:01 -0200

    first commit

---
+++ src/auth/Auth.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="auth">
+    Auth
+  </div>
+</template>
+
+<script>
+  export default {
+    // Do not forget this little guy
+    name: 'auth',
+    // share common functionality with component mixins
+    mixins: [],
+    // compose new components
+    extends: {},
+    // component properties/variables
+    props: {
+      bar: {}, // Alphabetized
+      foo: {},
+      fooBar: {}
+    },
+    // variables
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js PWA'
+      }
+    },
+    computed: {},
+    // when component uses other components
+    components: {},
+    // methods
+    watch: {},
+    methods: {},
+    // component Lifecycle hooks
+    beforeCreate () {},
+    mounted () {}
+}
+</script>
+
+<style scoped>
+  .auth { /* ... */ }
+</style>
